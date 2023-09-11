f3a860d491eb5f4b572a5978968555cb3ebc83de
Sankalp-G <sankalp.gawali@gmail.com>
2023-09-11 23:14:11 +0530

    add sample data and messages route

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
 router.get('/', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
+});
+
+router.get('/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
 router.post('/', (req, res) => {
   return res.send('POST HTTP method on user resource');
-});
-
-router.put('/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
 });
 
 router.delete('/:userId', (req, res) => {
